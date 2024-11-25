2a4a9a0404597ddbe4d5d3b32400f4c6b564baa4
hiyosha0 <101106462+hiyosha0@users.noreply.github.com>
2024-11-25 14:48:19 +0530

    footer_updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { FizziLogo } from "./Fizzylogo";
 import CircleText from "./CircleText";
 
@@ -7,10 +8,9 @@ type Props = {};
 export default function Footer({}: Props) {
   return (
     <footer className="bg-[#FEE832] text-[#FE6334]">
-      <div className="realtive mx-auto flex w-full max-w-4xl justify-center px-4 py-10">
+      <div className="relative mx-auto flex w-full max-w-4xl justify-center px-4 py-10">
         <FizziLogo />
         <div className="absolute right-24 top-0 size-28 origin-center -translate-y-14 md:size-48 md:-translate-y-28">
-          {/**Circle text */}
           <CircleText />
         </div>
       </div>
